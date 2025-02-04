33f67fc896343b680559d26497b94db7c6516b82
oxo4real <abduredoxo@hotmail.com>
2025-02-04 18:57:46 +0100

    added print_error2.c

---
+++ utils/print_error2.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   print_error2.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aaghzal <aaghzal@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/04 14:54:13 by aaghzal           #+#    #+#             */
+/*   Updated: 2025/02/04 18:57:27 by aaghzal          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putstr_fd(char const *s, int fd);
+
+void	print_error2(char *shell_name, char *command,
+		char *details, char *description)
+{
+	ft_putstr_fd(shell_name, STDERR_FILENO);
+	ft_putstr_fd(": ", STDERR_FILENO);
+	ft_putstr_fd(command, STDERR_FILENO);
+	ft_putstr_fd(": ", STDERR_FILENO);
+	ft_putstr_fd(details, STDERR_FILENO);
+	ft_putstr_fd(": ", STDERR_FILENO);
+	ft_putstr_fd(description, STDERR_FILENO);
+}
