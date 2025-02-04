11658261f9b6de473508a4090320f5f8f10ec8b7
oxo4real <abduredoxo@hotmail.com>
2025-02-04 18:42:13 +0100

    started using STDERR_FILENO for more portability and clarity

--- utils/print_error.c
+++ utils/print_error.c
@@ -6,19 +6,20 @@
 /*   By: aaghzal <aaghzal@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/04 14:54:13 by aaghzal           #+#    #+#             */
-/*   Updated: 2025/02/04 15:30:49 by aaghzal          ###   ########.fr       */
+/*   Updated: 2025/02/04 18:41:29 by aaghzal          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
+#include <unistd.h>
 
 void	ft_putstr_fd(char const *s, int fd);
 
 void	print_error(char *shell_name, char *command, char *details)
 {
-	ft_putstr_fd(shell_name, 2);
-	ft_putstr_fd(": ", 2);
-	ft_putstr_fd(command, 2);
-	ft_putstr_fd(": ", 2);
+	ft_putstr_fd(shell_name, STDERR_FILENO);
+	ft_putstr_fd(": ", STDERR_FILENO);
+	ft_putstr_fd(command, STDERR_FILENO);
+	ft_putstr_fd(": ", STDERR_FILENO);
 	perror(details);
 }
