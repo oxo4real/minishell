f4524201e478bdb79b324f201bcc061c51391f61
oxo4real <abduredoxo@hotmail.com>
2025-02-05 09:42:16 +0100

    fixed the order of checks in exit_

--- builtins/exit_.c
+++ builtins/exit_.c
@@ -6,7 +6,7 @@
 /*   By: aaghzal <aaghzal@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/04 16:19:06 by aaghzal           #+#    #+#             */
-/*   Updated: 2025/02/04 20:41:50 by aaghzal          ###   ########.fr       */
+/*   Updated: 2025/02/05 09:29:38 by aaghzal          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,13 +26,13 @@ void	exit_(char **av)
 	printf("exit\n");
 	if (count_args(av) == 1)
 		exit(0);
-	else if (count_args(av) > 2)
-		return (print_error2("minishell", "exit", NULL, "too many arguments"));
-	if (!is_valid(av[1]))
+	else if (!is_valid(av[1]))
 	{
 		print_error2("minishell", "exit", av[1], "numeric argument required");
 		exit(255);
 	}
+	else if (count_args(av) > 2)
+		return (print_error2("minishell", "exit", NULL, "too many arguments"));
 	exit(ft_atoi(av[1]));
 }
 
