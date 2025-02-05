553514da2965a852927a9ba92b04b511e8b17c7d
oxo4real <abduredoxo@hotmail.com>
2025-02-05 09:44:14 +0100

    fixed the order of checks in exit_

--- builtins/exit_.c
+++ builtins/exit_.c
@@ -6,12 +6,13 @@
 /*   By: aaghzal <aaghzal@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/04 16:19:06 by aaghzal           #+#    #+#             */
-/*   Updated: 2025/02/05 09:29:38 by aaghzal          ###   ########.fr       */
+/*   Updated: 2025/02/05 09:43:15 by aaghzal          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 int		ft_atoi(char *str);
 void	print_error2(char *shell_name, char *command,
@@ -21,9 +22,10 @@ int		ft_islong(char *s);
 static int	count_args(char **av);
 static int	is_valid(char *s);
 
-void	exit_(char **av)
+void	exit_(char **av, bool print)
 {
-	printf("exit\n");
+	if (print)
+		printf("exit\n");
 	if (count_args(av) == 1)
 		exit(0);
 	else if (!is_valid(av[1]))
@@ -50,6 +52,8 @@ static int	is_valid(char *s)
 {
 	char *tmp;
 
+	if (!s)
+		return (0);
 	tmp = s;
 	while (*tmp == ' ')
 		tmp++;
