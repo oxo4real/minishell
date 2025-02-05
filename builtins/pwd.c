fbfced3b10f712acc6bd8f2efe6a160667da1a30
oxo4real <abduredoxo@hotmail.com>
2025-02-05 15:58:08 +0100

    fixed pwd.c

--- builtins/pwd.c
+++ builtins/pwd.c
@@ -6,7 +6,7 @@
 /*   By: aaghzal <aaghzal@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/04 15:09:36 by aaghzal           #+#    #+#             */
-/*   Updated: 2025/02/04 15:31:37 by aaghzal          ###   ########.fr       */
+/*   Updated: 2025/02/05 15:55:54 by aaghzal          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,9 +21,8 @@ void	pwd(void)
 	char *cwd;
 
 	cwd = getcwd(NULL, 0);
-    if (cwd != NULL)
-        printf("%s\n", cwd);
+    if (!cwd)
+        return (print_error("minishell", "pwd", NULL), exit(EXIT_FAILURE));
     else
-        print_error("minishell", "pwd", NULL);
-	free(cwd);
+        return (printf("%s\n", cwd), free(cwd), exit(EXIT_SUCCESS));
 }
