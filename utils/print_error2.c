a9e6fdfed7e196248016156dbc1118abc0d5d714
oxo4real <abduredoxo@hotmail.com>
2025-02-05 15:27:37 +0100

    fixed some usues in print_error and print_error2

--- utils/print_error2.c
+++ utils/print_error2.c
@@ -6,7 +6,7 @@
 /*   By: aaghzal <aaghzal@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/04 14:54:13 by aaghzal           #+#    #+#             */
-/*   Updated: 2025/02/04 19:34:44 by aaghzal          ###   ########.fr       */
+/*   Updated: 2025/02/05 15:25:43 by aaghzal          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,14 @@ void	print_error2(char *shell_name, char *command,
 		char *details, char *description)
 {
 	ft_putstr_fd(shell_name, STDERR_FILENO);
-	ft_putstr_fd(": ", STDERR_FILENO);
+	if (shell_name && (command || details || description))
+		ft_putstr_fd(": ", STDERR_FILENO);
 	ft_putstr_fd(command, STDERR_FILENO);
-	ft_putstr_fd(": ", STDERR_FILENO);
+	if (command && (details || description))
+		ft_putstr_fd(": ", STDERR_FILENO);
 	ft_putstr_fd(details, STDERR_FILENO);
-	ft_putstr_fd(": ", STDERR_FILENO);
+	if (details && description)
+		ft_putstr_fd(": ", STDERR_FILENO);
 	ft_putstr_fd(description, STDERR_FILENO);
 	ft_putstr_fd("\n", STDERR_FILENO);
 }
