17735780aece10a24661b8e84ff00359bf9999a9
oxo4real <abduredoxo@hotmail.com>
2025-02-06 14:02:03 +0100

    added ft_strchr.c

---
+++ utils/ft_strchr.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   strchr.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/07 13:52:48 by marvin            #+#    #+#             */
+/*   Updated: 2024/08/07 13:52:48 by marvin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+char	*ft_strchr(const char *s, int c)
+{
+	int				i;
+	unsigned char	uc;
+
+	uc = (unsigned char)c;
+	i = 0;
+	while (s[i])
+	{
+		if (s[i] == uc)
+			return ((char *) &s[i]);
+		i++;
+	}
+	if (s[i] == uc)
+		return ((char *) &s[i]);
+	return (NULL);
+}
