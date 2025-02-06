dc88b3dc9b1dd1ce2671a0bc8d6fb0f569b27552
oxo4real <abduredoxo@hotmail.com>
2025-02-06 10:54:33 +0100

    added more protection in cd.c

--- builtins/cd.c
+++ builtins/cd.c
@@ -6,7 +6,7 @@
 /*   By: aaghzal <aaghzal@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/04 14:10:52 by aaghzal           #+#    #+#             */
-/*   Updated: 2025/02/06 10:22:19 by aaghzal          ###   ########.fr       */
+/*   Updated: 2025/02/06 10:54:20 by aaghzal          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@ void	print_error(char *shell_name, char *command, char *details);
 
 void	cd(char **av)
 {
+	if (!av)
+		return ;
 	if (chdir(av[1]) != 0)
 		print_error("minishell", "cd", av[1]);
 }
