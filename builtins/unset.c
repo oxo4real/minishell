db7577f0dfff8ddd4080b46af8715850246a68a0
oxo4real <abduredoxo@hotmail.com>
2025-02-06 13:16:48 +0100

    made unset able to unset _

--- builtins/unset.c
+++ builtins/unset.c
@@ -6,7 +6,7 @@
 /*   By: aaghzal <aaghzal@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/05 18:04:21 by aaghzal           #+#    #+#             */
-/*   Updated: 2025/02/06 11:39:22 by aaghzal          ###   ########.fr       */
+/*   Updated: 2025/02/06 13:16:02 by aaghzal          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,7 +37,7 @@ void	unset(char **av, t_env **env_lst)
 		var = (*env_lst);
 		while (var && ft_strcmp(var->key, av[i]) != 0)
 			var = var->next;
-		if (var && ft_strcmp(var->key, "_") != 0)
+		if (var)
 		{
 			head = (*env_lst);
 			while (head != var && head->next != var)
