02817f2cdfe670935a6e5dc0173319d7bf2b6051
oxo4real <abduredoxo@hotmail.com>
2025-02-06 13:19:51 +0100

    decided to put expandenvlst in it own seperate file in parsing/

--- parsing/expandenvlst.c
+++ parsing/expandenvlst.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   envtoenvlst.c                                      :+:      :+:    :+:   */
+/*   expandenvlst.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: aaghzal <aaghzal@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/02/05 11:38:54 by aaghzal           #+#    #+#             */
-/*   Updated: 2025/02/06 13:17:44 by aaghzal          ###   ########.fr       */
+/*   Created: 2025/02/06 13:17:59 by aaghzal           #+#    #+#             */
+/*   Updated: 2025/02/06 13:18:35 by aaghzal          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,28 +19,27 @@ typedef struct s_env
 	struct s_env	*next;
 }	t_env;
 
-char	**split_var(char *var);
-void	envlstclear(t_env **env_lst);
-int		expandenvlst(t_env **env_lst, char	**splited);
-
-t_env	*envtoenvlst(char **env)
+int	expandenvlst(t_env **env_lst, char	**splited)
 {
-	int		i;
-	char	**splited;
-	t_env	*env_lst;
+	t_env	*new;
+	t_env	*head;
 
-	env_lst = NULL;
-	i = 0;
-	while (env[i])
+	if (!env_lst || !splited)
+		return (0);
+	new = malloc(sizeof(t_env));
+	if (!new)
+		return (0);
+	new->key = splited[0];
+	new->value = splited[1];
+	new->next = NULL;
+	if (!(*env_lst))
+		(*env_lst) = new;
+	else
 	{
-		splited = split_var(env[i]);
-		if (!splited)
-			return (envlstclear(&env_lst), NULL);
-		if (!expandenvlst(&env_lst, splited))
-			return (free(splited[0]), free(splited[1]), free(splited),
-				envlstclear(&env_lst), NULL);
-		free(splited);
-		i++;
+		head = (*env_lst);
+		while (head->next)
+			head = head->next;
+		head->next = new;
 	}
-	return (env_lst);
+	return (1);
 }
