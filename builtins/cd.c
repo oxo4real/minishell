cc1383dd7b35a658b88c32b7d6200ca363c7d5e2
oxo4real <abduredoxo@hotmail.com>
2025-02-07 18:52:48 +0100

    made cd modify environment variables

--- builtins/cd.c
+++ builtins/cd.c
@@ -6,18 +6,60 @@
 /*   By: aaghzal <aaghzal@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/04 14:10:52 by aaghzal           #+#    #+#             */
-/*   Updated: 2025/02/06 10:54:20 by aaghzal          ###   ########.fr       */
+/*   Updated: 2025/02/07 18:46:08 by aaghzal          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdlib.h>
 #include <unistd.h>
+#include <stdio.h>
+
+typedef struct s_env
+{
+	char			*key;
+	char			*value;
+	struct s_env	*next;
+}	t_env;
 
 void	print_error(char *shell_name, char *command, char *details);
+int		ft_strcmp(const char *s1, const char *s2);
+int		expandenvlst(t_env **env_lst, char	**splited);
+void	export(char **av, t_env **env_lst);
+char	*ft_strdup(const char *s);
+char	*ft_strjoin(char const *s1, char const *s2, char const *sep);
 
-void	cd(char **av)
+void	set_env(t_env **env_lst);
+
+void	cd(char **av, t_env **env_lst)
 {
-	if (!av)
+	if (!av || !env_lst)
 		return ;
 	if (chdir(av[1]) != 0)
-		print_error("minishell", "cd", av[1]);
+		return (print_error("minishell", "cd", av[1]));
+	set_env(env_lst);
+}
+
+void	set_env(t_env **env_lst)
+{
+	t_env	*pwd;
+	char	*newpwd;
+	char	*av[3];
+
+	av[2] = NULL;
+	pwd = (*env_lst);
+	while (pwd && ft_strcmp(pwd->key, "PWD") != 0)
+		pwd = pwd->next;
+	if (!pwd || !pwd->value)
+		av[1] = ft_strdup("OLDPWD");
+	else
+		av[1] = ft_strjoin("OLDPWD", pwd->value, "=");
+	export(av, env_lst);
+	free(av[1]);
+	newpwd = getcwd(NULL, 0);
+	if (!newpwd)
+		av[1] = ft_strdup("PWD");
+	else
+		av[1] = ft_strjoin("PWD", newpwd, "=");
+	export(av, env_lst);
+	return (free(newpwd), free(av[1]));
 }
