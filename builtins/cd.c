26036f9d1127cd54e730c7356e9fe3913e940fe1
Abdellah Aghzal <aaghzal@c2r2p1.1337.ma>
2025-02-07 20:19:43 +0100

    made cd go home if it has no arguments

--- builtins/cd.c
+++ builtins/cd.c
@@ -6,7 +6,7 @@
 /*   By: aaghzal <aaghzal@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/04 14:10:52 by aaghzal           #+#    #+#             */
-/*   Updated: 2025/02/07 18:46:08 by aaghzal          ###   ########.fr       */
+/*   Updated: 2025/02/07 20:14:12 by aaghzal          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,16 +27,35 @@ int		expandenvlst(t_env **env_lst, char	**splited);
 void	export(char **av, t_env **env_lst);
 char	*ft_strdup(const char *s);
 char	*ft_strjoin(char const *s1, char const *s2, char const *sep);
+void	print_error2(char *shell_name, char *command,
+		char *details, char *description);
 
+int		gohome(t_env **env_lst);
 void	set_env(t_env **env_lst);
 
 void	cd(char **av, t_env **env_lst)
 {
 	if (!av || !env_lst)
 		return ;
-	if (chdir(av[1]) != 0)
+	else if (!av[1] && !gohome(env_lst))
+		return ;
+	else if (av[1] && chdir(av[1]) != 0)
 		return (print_error("minishell", "cd", av[1]));
 	set_env(env_lst);
+}
+
+int		gohome(t_env **env_lst)
+{
+	t_env	*home;
+
+	home = (*env_lst);
+	while (home && ft_strcmp(home->key, "HOME") != 0)
+		home = home->next;
+	if (!home || !home->value)
+		return (print_error2("minishell", "cd", NULL, "HOME not set"), 0);
+	if (chdir(home->value) != 0)
+		return (print_error("minishell", "cd", home->value), 0);
+	return (1);
 }
 
 void	set_env(t_env **env_lst)
