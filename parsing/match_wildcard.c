cbd7bd2fb52f92a49c7d5b1864dcae9fdf789197
oxo4real <abduredoxo@hotmail.com>
2025-02-16 19:03:58 +0100

    made match_wildcard handle single and double quotes

--- parsing/match_wildcard.c
+++ parsing/match_wildcard.c
@@ -6,13 +6,14 @@
 /*   By: aaghzal <aaghzal@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/10 11:53:01 by aaghzal           #+#    #+#             */
-/*   Updated: 2025/02/10 13:37:28 by aaghzal          ###   ########.fr       */
+/*   Updated: 2025/02/16 18:48:40 by aaghzal          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 
 static int	checkallstars(const char *p);
+static int	the_damn_qotes(char **p, char **s);
 
 int	match_wildcard(char *s, char *p)
 {
@@ -22,14 +23,15 @@ int	match_wildcard(char *s, char *p)
 	last_star = NULL;
 	while (*s)
 	{
-		if (*p == *s && p++ && s++)
-			continue ;
-		else if (*p == '*')
+		if (*p == '*')
 		{
-			last_star = p;
+			last_star = p++;
 			tmp = s;
-			p++;
 		}
+		else if ((*p == '"' || *p == '\'') && the_damn_qotes(&p, &s))
+			continue ;
+		else if (*p == *s && p++ && s++)
+			continue ;
 		else if (!last_star)
 			return (0);
 		else
@@ -50,5 +52,29 @@ static int	checkallstars(const char *p)
 			return (0);
 		p++;
 	}
+	return (1);
+}
+
+static int	the_damn_qotes(char **p, char **s)
+{
+	char	delim;
+	char	*pcp;
+	char	*scp;
+	int		increment;
+
+	increment = 0;
+	pcp = *p;
+	scp = *s;
+	delim = *pcp;
+	pcp++;
+	while (*pcp != delim)
+	{
+		
+		if (*(pcp++) != *(scp++))
+			return (0);
+		increment++;
+	}
+	(*p) += increment + 2;
+	(*s) += increment;
 	return (1);
 }
