8c717ca759a28e0e91c0ce5beaa5cf8ff2fa874d
oxo4real <abduredoxo@hotmail.com>
2025-02-17 13:12:04 +0100

    made generaterandstr start with a dot while generating a rand str

--- utils/generaterandstr.c
+++ utils/generaterandstr.c
@@ -6,7 +6,7 @@
 /*   By: aaghzal <aaghzal@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/08 12:07:29 by aaghzal           #+#    #+#             */
-/*   Updated: 2025/02/08 12:44:59 by aaghzal          ###   ########.fr       */
+/*   Updated: 2025/02/17 13:01:12 by aaghzal          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@
 
 #define MAX_SIZE 50
 #define MIN_SIZE 10
+
+char	*ft_strjoin(char const *s1, char const *s2, char const *sep);
 
 char	*generaterandstr(void)
 {
@@ -41,5 +43,5 @@ char	*generaterandstr(void)
 		free(allocated);
 		rendu[length] = 97 + (rand % (122 - 97 + 1));
 	}
-	return (rendu);
+	return (ft_strjoin(".", rendu, ""));
 }
