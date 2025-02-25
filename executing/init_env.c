f9bb40e5a0fae9f9975f7e74217236a01c4b0703
oxo4real <abduredoxo@hotmail.com>
2025-02-25 11:48:50 +0000

    added init_env.c in executing

--- executing/init_env.c
+++ executing/init_env.c
@@ -6,7 +6,7 @@
 /*   By: aaghzal <aaghzal@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/25 11:31:28 by aaghzal           #+#    #+#             */
-/*   Updated: 2025/02/25 11:40:36 by aaghzal          ###   ########.fr       */
+/*   Updated: 2025/02/25 11:48:44 by aaghzal          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,9 @@
 
 void	init_env(t_env **env_lst)
 {
-	char	*av[2];
+	char	*av[3];
 
+	av[2] = NULL;
 	set_shlvl(env_lst);
 	av[1] = ".";
 	cd(av, env_lst);
