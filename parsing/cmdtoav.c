c2ac0ea344b118076a40885102f05497881f6c15
oxo4real <abduredoxo@hotmail.com>
2025-02-25 15:13:51 +0000

    changed how the cmdtoav works

--- parsing/cmdtoav.c
+++ parsing/cmdtoav.c
@@ -3,19 +3,69 @@
 /*                                                        :::      ::::::::   */
 /*   cmdtoav.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mhayyoun <mhayyoun@student.42.fr>          +#+  +:+       +#+        */
+/*   By: aaghzal <aaghzal@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/24 18:59:26 by aaghzal           #+#    #+#             */
-/*   Updated: 2025/02/24 20:20:05 by mhayyoun         ###   ########.fr       */
+/*   Updated: 2025/02/25 15:11:34 by aaghzal          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "parsing.h"
 
+static void	removemainqotes(char *str);
+static void	replace(char *str, int *j);
+
 char	**cmdtoav(char **cmd, t_env *env_lst)
 {
+	char	**av;
+	int		i;
+
 	replaceenvar(env_lst, cmd);
 	sptobel(*cmd);
 	replacewildcards(cmd);
-	return (ft_split(*cmd, SEP));
+	av = ft_split(*cmd, SEP);
+	if (!av)
+		return (NULL);
+	i = 0;
+	while (av[i])
+		removemainqotes(av[i++]);
+	return (av);
+}
+
+static void	removemainqotes(char *str)
+{
+	int	j;
+
+	if (!str)
+		return ;
+	j = 0;
+	replace(str, &j);
+	while (str[j])
+		str[j++] = '\0';
+}
+
+static void	replace(char *str, int *j)
+{
+	int	i;
+	int	in_sq;
+	int	in_dq;
+
+	i = 0;
+	in_dq = 0;
+	in_sq = 0;
+	while (str[i] && str[i] != SEP)
+	{
+		if ((str[i] != '\'' && str[i] != '"') || (str[i] == '\'' && in_dq)
+			|| (str[i] == '"' && in_sq))
+			str[(*j)++] = str[i];
+		if (str[i] == '\'' && !in_sq && !in_dq)
+			in_sq = 1;
+		else if (str[i] == '\'' && in_sq)
+			in_sq = 0;
+		if (str[i] == '"' && !in_dq && !in_sq)
+			in_dq = 1;
+		else if (str[i] == '"' && in_dq)
+			in_dq = 0;
+		i++;
+	}
 }
--- parsing/replacewildcards.c
+++ parsing/replacewildcards.c
@@ -6,15 +6,15 @@
 /*   By: aaghzal <aaghzal@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/18 16:45:12 by aaghzal           #+#    #+#             */
-/*   Updated: 2025/02/24 20:08:27 by aaghzal          ###   ########.fr       */
+/*   Updated: 2025/02/25 15:12:41 by aaghzal          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "parsing.h"
 
 static void	replacewildcard(char **str, char *tmp, int *i);
-static void	removemainqotes(char *str);
-static void	replace(char *str, int *j);
+//static void	removemainqotes(char *str);
+//static void	replace(char *str, int *j);
 
 void	replacewildcards(char **str)
 {
@@ -39,7 +39,7 @@ static void	replacewildcard(char **str, char *tmp, int *i)
 		(*i)--;
 	sep = getsortedwildcard(&(*str)[(*i) + 1]);
 	if (!sep)
-		return (removemainqotes(&(*str)[(*i) + 1]));
+		return ;//(removemainqotes(&(*str)[(*i) + 1]));
 	if ((*i) > 0)
 		(*str)[*i] = '\0';
 	else
@@ -47,45 +47,45 @@ static void	replacewildcard(char **str, char *tmp, int *i)
 	rendu = ft_strjoin((*str), tmp + 1, sep);
 	free(sep);
 	if (!rendu)
-		return (removemainqotes(&(*str)[(*i) + 1]));
+		return ;//(removemainqotes(&(*str)[(*i) + 1]));
 	free(*str);
 	(*str) = rendu;
 }
 
-static void	removemainqotes(char *str)
-{
-	int	j;
+//static void	removemainqotes(char *str)
+//{
+//	int	j;
 
-	if (!str)
-		return ;
-	j = 0;
-	replace(str, &j);
-	while (str[j] && str[j] != SEP)
-		str[j++] = SEP;
-}
+//	if (!str)
+//		return ;
+//	j = 0;
+//	replace(str, &j);
+//	while (str[j] && str[j] != SEP)
+//		str[j++] = SEP;
+//}
 
-static void	replace(char *str, int *j)
-{
-	int	i;
-	int	in_sq;
-	int	in_dq;
+//static void	replace(char *str, int *j)
+//{
+//	int	i;
+//	int	in_sq;
+//	int	in_dq;
 
-	i = 0;
-	in_dq = 0;
-	in_sq = 0;
-	while (str[i] && str[i] != SEP)
-	{
-		if ((str[i] != '\'' && str[i] != '"') || (str[i] == '\'' && in_dq)
-			|| (str[i] == '"' && in_sq))
-			str[(*j)++] = str[i];
-		if (str[i] == '\'' && !in_sq && !in_dq)
-			in_sq = 1;
-		else if (str[i] == '\'' && in_sq)
-			in_sq = 0;
-		if (str[i] == '"' && !in_dq && !in_sq)
-			in_dq = 1;
-		else if (str[i] == '"' && in_dq)
-			in_dq = 0;
-		i++;
-	}
-}
+//	i = 0;
+//	in_dq = 0;
+//	in_sq = 0;
+//	while (str[i] && str[i] != SEP)
+//	{
+//		if ((str[i] != '\'' && str[i] != '"') || (str[i] == '\'' && in_dq)
+//			|| (str[i] == '"' && in_sq))
+//			str[(*j)++] = str[i];
+//		if (str[i] == '\'' && !in_sq && !in_dq)
+//			in_sq = 1;
+//		else if (str[i] == '\'' && in_sq)
+//			in_sq = 0;
+//		if (str[i] == '"' && !in_dq && !in_sq)
+//			in_dq = 1;
+//		else if (str[i] == '"' && in_dq)
+//			in_dq = 0;
+//		i++;
+//	}
+//}
