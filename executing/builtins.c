bf3274069ee9bf6ed8bc13662f6d8e0f160d5c04
Hayoun01 <3aba9irablog@gmail.com>
2025-02-26 10:36:47 +0000

    fix

--- executing/builtins.c
+++ executing/builtins.c
@@ -3,33 +3,32 @@
 /*                                                        :::      ::::::::   */
 /*   builtins.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aaghzal <aaghzal@student.42.fr>            +#+  +:+       +#+        */
+/*   By: mhayyoun <mhayyoun@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/23 18:54:22 by mhayyoun          #+#    #+#             */
-/*   Updated: 2025/02/25 19:40:10 by aaghzal          ###   ########.fr       */
+/*   Updated: 2025/02/26 10:36:17 by mhayyoun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "builtins.h"
-#include "executing.h"
 
 bool	builtins(t_node *head, t_exec *x)
 {
+	if (!head || !x)
+		return (0);
 	if (ft_strcmp("echo", head->args[0]) == 0)
 		return (echo(head->args, x), 1);
 	else if (ft_strcmp("export", head->args[0]) == 0)
 	{
 		_export(head->args, &x->lst, x);
 		freestrarr(&x->env);
-		x->env = envlsttoenv(x->lst);
-		return (1);
+		return (x->env = envlsttoenv(x->lst), 1);
 	}
 	else if (ft_strcmp("unset", head->args[0]) == 0)
 	{
 		unset(head->args, &x->lst, x);
 		freestrarr(&x->env);
-		x->env = envlsttoenv(x->lst);
-		return (1);
+		return (x->env = envlsttoenv(x->lst), 1);
 	}
 	else if (ft_strcmp("cd", head->args[0]) == 0)
 		return (cd(head->args, &x->lst, x), 1);
